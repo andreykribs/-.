53f6d41b5aa8d540cd4229584de1e9ee232c8361
andreykribs <andreykribs@mail.ru>
2024-08-27 22:14:30 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string = input()
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" ",""))
+print(my_string [:1])
+print(my_string [-1:])
+
+#я использовал свои слова так как в задании было написано слово "urban" но оно одно, а нужно два слова, чтобы получилось без пробелов, поэтому я использовал "привет мир"
